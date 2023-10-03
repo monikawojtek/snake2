08001e882668f328c77a755a2f1a8d8e74c393ed
monikawojtek <>
2023-10-03 15:56:53 +0000

    fisrt

--- run.py
+++ run.py
@@ -1,112 +1,123 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# This is a simple snake game program
+#Te rzeczy możesz zmieniać, aby uzyskiwać różne układy :)
+
+Rzm = 800 #rozmiar okna: Rzm x Rzm (domyslnie 800x800)
+s = 40 #rozmiar planszy: s x s, Rzm musi byc podzielne przez s (inaczej moze byc krzywo albo nie pokazywac czegos albo cos)
+Seed = "Zwykły Seed" #seed do losowania - liczba albo string
+Kratki = 800 #ile kratek bedzie poczatkowo "zywych" - musi byc mniej, niz s^2 (inaczej pokaze ValueError)
+czas = 50 #ile milisekund czekac do zrobienia nowej generacji
+#Uwaga - podany czas to czas po akualizacji i narysowniu kolejnej generacji - wiec nalezy uwzglednic, ze realny czas zawsze bedzie (czasami duzo) wiekszy
+Pattern = "23/3" #Przed ukośnikiem umieszcza się ilości żywych sąsiadów, aby komórka nie umarła, a po ukośniku ilość żywych sąsiadów, aby martwa komórka ożyła
+#Domyślny pattern oryginalnej "Gry w Życie" wg reguł Conwaya to "23/3"
+
+
+
+#Nizej faktyczny kod
+
 
 import tkinter as tk
+import random as rnd
 
-import random as rn
 
-def main():
-    # snake speed
-    dt = 200 
-    
-    # create window
-    window = tk.Tk() 
-    
-    # window title
-    window.title("Snake") 
-    
-    # create canvas
-    c_draw = tk.Canvas(window, width = 600, height = 400) 
-   
-   # pack canvas
-    c_draw.pack() 
-    
-    # snake class
-    class Snake: 
-        def __init__(self, width, height):
-            self.width = int(width) # area width
-            self.height = int(height) # area height
-            self.msnake = [[self.width / 2, self.height / 2], [self.width / 2 + 1, self.height / 2], [self.width / 2 + 2, self.height / 2]] # snake elements
-            self.move = 0 # index of vector described direction of snake move
-            self.tmove = [[0,1],[1,0],[0,-1],[-1,0]] # vectors of direacion of snake move
-            self.size = 10
-            self.col = False # określa, czy doszło do kolizji
-            self.food = [rn.randint(1, self.width - 2), rn.randint(1, self.height - 2)]
-        def drawBox(self,x, y, color = 'green'):
-            c_draw.create_rectangle([x, y, x + self.size, y + self.size], fill=color)
-        def draw(self):
-            c_draw.delete("all")
-            if self.col:
-                c_draw.create_text([self.width / 2 * self.size, self.height / 2 * self.size], text = "Przegrałeś")
-            else:
-                for i in range(self.width):
-                    self.drawBox(i * self.size, 0)
-                    self.drawBox(i * self.size, (self.height - 1) * self.size)
-                for i in range(1, self.height):
-                    self.drawBox(0, i * self.size)
-                    self.drawBox((self.width - 1) * self.size, i * self.size)
-                for i in self.msnake:
-                    self.drawBox(i[0] * self.size, i[1] * self.size)
-                self.drawBox(self.food[0] * self.size, self.food[1] * self.size, color = 'red')
-        def eat(self): # snake must eat
-            if self.msnake[0][0] == self.food[0] and self.msnake[0][1] == self.food[1]: # if head of snake is on food, then snake eat
-                self.msnake.append([0,0]) # add snake element
-                self.food = [rn.randint(1, self.width - 2), rn.randint(1, self.height - 2)] # random new food for snake
-        def move_snake(self):
-            for i in range(len(self.msnake) - 1,0,-1):
-                self.msnake[i][0] = self.msnake[i-1][0]
-                self.msnake[i][1] = self.msnake[i-1][1]
-            self.msnake[0][0] += self.tmove[self.move][0]
-            self.msnake[0][1] += self.tmove[self.move][1]
-            self.colision()
-            self.eat()
-            self.draw()
-        def turnLeft(self):
-            self.move = (self.move + 1) % len(self.tmove)
-        def turnRight(self):
-            self.move = (self.move - 1) if self.move > 0 else len(self.tmove) - 1
-        def colision(self):
-            if self.msnake[0][0] == 0 or self.msnake[0][1] == 0 or self.msnake[0][0] == self.width - 1 or self.msnake[0][1] == self.height - 1:
-                self.col = True
-            for i in self.msnake[1:]: # colision snake - snake
-                if self.msnake[0][0] == i[0] and self.msnake[0][1] == i[1]:
-                    self.col = True
-        def reset(self):
-            self.col = False
-            self.msnake = [[self.width / 2, self.height / 2], [self.width / 2 + 1, self.height / 2], [self.width / 2 + 2, self.height / 2]] # snake elements
-            self.move = 0 # index of vector described direction of snake move
-            self.tmove = [[0,1],[1,0],[0,-1],[-1,0]] # vectors of direacion of snake move
-            self.size = 10
-            self.col = False # określa, czy doszło do kolizji
-            self.food = [rn.randint(1, self.width - 2), rn.randint(1, self.height - 2)]
-            window.after(dt, move)
-    
-    sn = Snake(600 / 10, 400 / 10)
-    
-    menubar = tk.Menu(window)
-    menu = tk.Menu(menubar, tearoff=0)
-    menubar.add_cascade(label="Program", menu=menu)
-    menu.add_command(label="Od nowa", command = sn.reset)
-    menu.add_command(label="Wyjście", command = window.quit)
-    window.config(menu=menubar)
-    
-    sn.draw()
-    def move():
-        sn.move_snake()
-        if not sn.col:
-            window.after(dt, move)
-    def turnLeft(event):
-        sn.turnLeft()
-    def turnRight(event):
-        sn.turnRight()
-    window.after(dt, move)
-    window.bind_all("<KeyPress-Left>", turnLeft)
-    window.bind_all("<KeyPress-Right>", turnRight)
-    
-    window.mainloop()
-    
-    return 0
+#Klasa okna:
+class Application:
+    def __init__(self, rozmiar):
+        self.Arr = [ [False for x in range(s)] for y in range(s) ] #Tablica o wymiarach s x s, poczatkowo falsz (martwa)
 
-if __name__ == '__main__':
-    main()
+        self.window = tk.Tk()
+        self.window.resizable(width = False, height = False)
+        self.window.geometry(rozmiar)
+        self.window.title("Game of Life")
+
+        self.canvas = tk.Canvas(self.window, width = Rzm, height = Rzm)
+        self.canvas.pack()
+
+        self.createArray()
+
+        #Tutaj sa ladne sobie kwadraty ktore beda sobie malowane
+        self.Rct = [ [self.canvas.create_rectangle(x*krSz, y*krSz, krSz*(x+1), krSz*(y+1), fill="white") for x in range(s)] for y in range(s) ]
+
+        self.updated()
+
+        self.window.mainloop()
+
+    #Funckja do uzupelnienia tablicy
+    def createArray(self):
+        #Wybiera *Kratki* liczb od 0 do s*s i odpowiednie miejsca w tablicy daje tam prawde (zywa)
+        for z in rnd.sample(range(s*s), Kratki):
+            self.Arr[z%s][z//s] = True
+
+        #vvv tutaj byly customowe figury
+        #self.Arr[1][0] = True
+        #self.Arr[2][1] = True
+        #self.Arr[2][2] = True
+        #self.Arr[1][2] = True
+        #self.Arr[0][2] = True
+
+
+    #Dla kratki o koord. x, y oblicza, ile jest zywych dookola niej
+    def liczObok(self, x, y):
+        #Mniejsze i wieksze pola - przejscia, jezeli wychodza poza granice: (ifFalse, ifTrue)[condition]
+        xm = ( x-1, s-1 )[ (x-1) == -1 ]
+        xw = ( x+1, 0   )[ (x+1) ==  s ]
+        ym = ( y-1, s-1 )[ (y-1) == -1 ]
+        yw = ( y+1, 0   )[ (y+1) ==  s ]
+        #Zwrocenie liczby pol
+        wynik  = int(self.Arr[xm][ym]) + int(self.Arr[x][ym]) + int(self.Arr[xw][ym])
+        wynik += int(self.Arr[xm][y])  +          0           + int(self.Arr[xw][y])
+        wynik += int(self.Arr[xm][yw]) + int(self.Arr[x][yw]) + int(self.Arr[xw][yw])
+
+        return wynik
+
+
+    #Funkcja update
+    def updated(self):
+
+        #Rysowanie
+        for x in range(s):
+            for y in range(s):
+                if self.Arr[x][y]:
+                    self.canvas.itemconfig(self.Rct[x][y], fill="black") #Jeżeli jest żywe, robi czarne
+                else:
+                    self.canvas.itemconfig(self.Rct[x][y], fill="white") #A jak nie, to bialy
+
+
+        #Tymczasowa tablica zawierajaca odswiezone stany
+        OdswArr = [ [False for x in range(s)] for y in range(s) ]
+
+        #Uzupelnienie tymczasowej tablicy nowa warstwa
+        for x in range(s):
+            for y in range(s):
+                sasiedzi = self.liczObok(x, y)
+
+                if self.Arr[x][y]:              #Pozostawanie przy żywych - jezeli licba sasiadow jest w "stay"
+                    if sasiedzi in stay:
+                        OdswArr[x][y] = True
+                else:                           #Jeżeli nie jesteś żywy, ale liczba sasiadow jest w "born" - ożyj
+                    if sasiedzi in born:
+                        OdswArr[x][y] = True
+
+        #Podmiana tablicy
+        self.Arr = OdswArr.copy()
+
+        #Odpalenie ponownie po *podanej ilosci* ms
+        self.window.after(czas, self.updated)
+
+
+
+#Aktualne dzialanie programu - koniec definicji
+
+
+krSz = Rzm/s #Wymiar jednej kratki - zeby bylo prosciej pisac
+
+rnd.seed(Seed)
+
+stTab, bnTab = Pattern.split("/") #Dzielenie patternu na 2 czesci
+#Zmienienie tych czesci na liczby
+stay = [int(x) for x in stTab] #To jest ile ma być obok, aby przeżyć
+born = [int(x) for x in bnTab] #To jest ile ma być obok, aby ożyć
+
+#String rozmiaru okna
+rozmiar = str(Rzm) + "x" + str(Rzm)
+
+#Tworzenie obiektu aplikacji
+apl = Application(rozmiar)
